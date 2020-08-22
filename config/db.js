6e1d7cb7a185b16812717ca995fd494b122a0831
lshishkin <l.shishkin@kari.com>
2020-08-22 18:46:22 +0500

    debug(app): изменен порт

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-// Replace this with your MONGOURI.
 const MONGOURI = "mongodb://admin:12345q@ds145923.mlab.com:45923/react-redux";
 
 const InitiateMongoServer = async () => {
   try {
     await mongoose.connect(MONGOURI, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log("Connected to DB !!");
   } catch (e) {
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 
 // PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
